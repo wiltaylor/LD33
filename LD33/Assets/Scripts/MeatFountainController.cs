129289021a828fcff0966ef7d1c1ff3ca42741a5
Wil Taylor <wilfridtaylor@gmail.com>
2015-08-23 23:37:13 +1000

    Cleaning up project. Added Meat fountain.

---
+++ LD33/Assets/Scripts/MeatFountainController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeatFountainController : MonoBehaviour
+{
+
+    public float Duration = 1f;
+
+	// Use this for initialization
+	void Start () {
+	    Destroy(gameObject, Duration);
+	}
+}
---
+++ LD33/Assets/Scripts/ParticleSystemLayerFix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemLayerFix : MonoBehaviour
+{
+    public string SortingLayer = "";
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var part = GetComponent<Renderer>();
+	    part.sortingLayerName = "Unit";
+	}
+	
+}
